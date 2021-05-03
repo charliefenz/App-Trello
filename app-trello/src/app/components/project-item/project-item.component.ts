206334711a428918679343661f1b937300f9e868
charliefenz <fentecarlos@gmail.com>
2021-05-03 08:00:09 +0200

    created task creation bar

--- app-trello/src/app/components/project-item/project-item.component.ts
+++ app-trello/src/app/components/project-item/project-item.component.ts
@@ -1,5 +1,7 @@
 import { Component, Input, OnInit } from '@angular/core';
 import { Project } from 'src/app/Models/project';
+import { Task } from 'src/app/Models/task';
+import { NgForm } from '@angular/forms';
 
 @Component({
   selector: 'app-project-item',
@@ -10,9 +12,32 @@ export class ProjectItemComponent implements OnInit {
 
   @Input()
   project: Project;
+
+  newTaskPlaceholder = 'Agregar una nueva tarea...';
+  newTaskValue = null;
+  newTaskFocused = false;
   constructor() { }
 
   ngOnInit(): void {
+  }
+
+  newTaskFocus(): void {
+     this.newTaskPlaceholder = '';
+     this.newTaskFocused = true;
+  }
+
+  createTask(taskCreateForm: NgForm): void {
+    const task: Task = {
+      name: taskCreateForm.form.value.newTask,
+      priority: 1,
+      creationDate: new Date(),
+      dueDate: new Date(),
+      completed: false
+    };
+    this.project.tasks.push(task);
+    this.newTaskValue = null;
+    this.newTaskFocused = false;
+    this.newTaskPlaceholder = 'Agregar una nueva tarea...';
   }
 
 }
